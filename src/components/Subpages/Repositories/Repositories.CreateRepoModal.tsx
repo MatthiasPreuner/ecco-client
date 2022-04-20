2abac5eb13943be39be539218861a79baa5a76ac
MatthiasPreuner <matthias.preuner@gmail.com>
2022-04-20 11:18:00 +0200

    form validation added

--- src/components/Subpages/Repositories/Repositories.CreateRepoModal.tsx
+++ src/components/Subpages/Repositories/Repositories.CreateRepoModal.tsx
@@ -8,33 +8,43 @@ import { useSharedState } from "../../../states/AppState";
 export const CreateRepoModal: React.FC = () => {
 
   const [show, setShow] = useState(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [appState, setAppState] = useSharedState();
+  const [validated, setValidated] = useState(false);
 
-  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  let [inputValue, setInputValue] = useState<string>(null);
-  let [appState, setAppState] = useSharedState();
-
-  const setValueInAppState = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  let onModalDismiss = () => {
-    setInputValue(null);
-    handleClose();
+  const handleClose = () => {
+    // clear form
+    setInputValue("");
+    setValidated(false);
+    setShow(false);
   }
 
-  let createRepo = () => {
+  let nameIsValid = () => inputValue.length > 0 && appState.availableRepositories.filter(v => v.name.toLowerCase() === inputValue.toLowerCase()).length < 1;
+
+  let createRepo = (event: React.FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false || !nameIsValid()) {
+
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+
+    } else {
+
       CommunicationService.getInstance().createRepository(inputValue).then((apiData: RepositoryHeaderResponse) => {
-          console.log(apiData.data);
-          setAppState((previousState) => ({
-              ...previousState,
-              availableRepositories: apiData.data
-          }));
+        console.log(apiData.data);
+        setAppState((previousState) => ({
+          ...previousState,
+          availableRepositories: apiData.data
+        }));
       });
-    setInputValue(null);
-    handleClose();
-  }
+
+      handleClose();
+    }
+  };
 
   return (
     <>
@@ -52,19 +62,29 @@ export const CreateRepoModal: React.FC = () => {
         <Modal.Header closeButton>
           <Modal.Title>Create Repository</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-
-          <Form className="w-80">
+        <Form className="w-80" noValidate validated={validated} onSubmit={createRepo}>
+          <Modal.Body>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>A new empty Repository will be created. Please enter a name:</Form.Label>
-              <Form.Control type="text" placeholder="Name of the new Repository..." pattern="[A-Za-z0-9_]{1,}" value={inputValue} onChange={setValueInAppState}/>
+              <Form.Control
+                type="text"
+                isInvalid={!nameIsValid()}
+                isValid={nameIsValid()}
+                placeholder="Name of the new Repository..."
+                pattern="[A-Za-z0-9_]{1,}" value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)} />
+              {validated ?
+                (inputValue?.length < 1 ?
+                <Form.Control.Feedback type="invalid">Name must not be empty!</Form.Control.Feedback> :
+                <Form.Control.Feedback type="invalid">Name already exists!</Form.Control.Feedback>) : null
+              }
             </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={onModalDismiss}>Close</Button>
-          <Button variant="primary" type="submit" onClick={createRepo}>Create Repository</Button>
-        </Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>Close</Button>
+            <Button variant="primary" type="submit">Create Repository</Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   );
