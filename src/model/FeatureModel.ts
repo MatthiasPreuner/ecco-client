d56cf7310f37edd8b3547fe9991c91fef95a7b73
Matthias Preuner <matthias.preuner@gmail.com>
2022-04-25 15:49:18 +0200

    old testing data removed

--- src/model/FeatureModel.ts
+++ src/model/FeatureModel.ts
@@ -7,35 +7,3 @@ export interface FeatureModel {
     revisions: FeatureRevisionModel[],
 /*     latestRevision: FeatureRevisionModel */
 }
-
-export const FeatureModelDefault: FeatureModel[] = [{
-    id: '1',
-    description: "desc",
-    name: "person",
-    revisions: []
-},
-{
-    id: '2',
-    description: "desc",
-    name: "purpleshirt",
-    revisions: []
-},
-{
-    id: '3',
-    description: "desc",
-    name: "stripedshirt",
-    revisions: []
-},
-{
-    id: '4',
-    description: "desc",
-    name: "jacket",
-    revisions: []
-},
-{
-    id: '5',
-    description: "desc",
-    name: "glasses",
-    revisions: []
-}
-];
