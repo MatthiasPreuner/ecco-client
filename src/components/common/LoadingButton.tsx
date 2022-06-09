f5d655e81fef0efdcf3a75183b3ef8a14861cc39
MatthiasPreuner <matthias.preuner@gmail.com>
2022-06-09 16:09:15 +0200

    LoadingButton with Spinner created

---
+++ src/components/common/LoadingButton.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Button, ButtonProps, Spinner } from "react-bootstrap";
+
+export interface ErrorResponseToastProps {
+    loading?: boolean,
+}
+
+export const LoadingButton: React.FC<ErrorResponseToastProps & ButtonProps> = (props) => {
+
+    return (
+        <Button {...props} disabled={props.loading}>
+            {props.loading &&
+                <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                />}
+            &nbsp;
+            {props.children}
+        </Button>
+    )
+}
