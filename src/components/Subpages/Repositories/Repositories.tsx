d008171ab3bd18bd7aece739cdb80d58697911d0
MatthiasPreuner <matthias.preuner@gmail.com>
2022-06-09 18:03:22 +0200

    Loading Spinner added + Feedback improved

--- src/components/Subpages/Repositories/Repositories.tsx
+++ src/components/Subpages/Repositories/Repositories.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { AppState, useSharedState } from "../../../states/AppState";
+import { useSharedState } from "../../../states/AppState";
 import { CommunicationService } from "../../../services/CommunicationService";
 import { useNavigate } from 'react-router-dom';
 
-import { Button, ButtonGroup, Container, Row, Col, ListGroup, InputGroup, FormControl } from 'react-bootstrap';
+import { Button, ButtonGroup, Container, Row, Col, ListGroup, InputGroup, FormControl, Spinner } from 'react-bootstrap';
 
 import { CreateRepoModal } from "./Repositories.CreateRepoModal";
 import { DeleteRepoModal } from "./Repositories.DeleteRepoModal";
@@ -14,13 +14,16 @@ import { RepositoryResponse } from "../../../model/RepositoryResponse";
 import { CloneRepoModal } from "./Repositories.CloneRepoModal";
 import { RepositoryHeaderModel } from "../../../model/RepositoryModel";
 import { RepositoryHeaderResponse } from "../../../model/AvailableRepositoryResponse";
-
+import { LoadingButton } from "../../common/LoadingButton";
 
 export const Repositories: React.FC = () => {
 
     const [appState, setAppState] = useSharedState();
     const [selectedRepo, setSelectedRepo] = useState<RepositoryHeaderModel>(null);
     const [repositoryFilterText, setRepositoryFilterText] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [choosing, setChoosing] = useState<boolean>(false);
+
 
     const navigate = useNavigate();
 
@@ -33,36 +36,46 @@ export const Repositories: React.FC = () => {
     }, [appState.userIsLoggedIn]);
 
     let refresh = () => {
+        setLoading(true)
         CommunicationService.getInstance().getAllRepositories().then((apiData: RepositoryHeaderResponse) => {
             setAppState((previousState) => ({
                 ...previousState,
                 availableRepositories: apiData.data
             }));
-        });
+            setLoading(false)
+        }, (e) => setLoading(false)); // TODO add error Toast?
     }
 
     let chooseRepo = () => {
+        setChoosing(true)
         CommunicationService.getInstance().getRepository(selectedRepo).then((apiData: RepositoryResponse) => {
             setAppState((previousState) => ({
                 ...previousState,
                 repository: apiData.data
             }));
-        });
+            setChoosing(false)
+        }, (e) => setChoosing(false));
     }
 
     const getCurrentRepositoryExpression = (): JSX.Element[] => {
-        return appState.availableRepositories?.map((repository: RepositoryHeaderModel, i) => {
-            if (repository.name.toLowerCase().includes(repositoryFilterText.toLowerCase())) {
-                return (
-                    <ListGroup.Item key={i} action active={repository === selectedRepo} onClick={() => setSelectedRepo(repository)}>{repository.rid === appState.repository?.rid && <i className="bi bi-dot"></i>} {repository.name}</ListGroup.Item>
-                );
-            }
-        }).filter((singleJSXElement: JSX.Element) => {
-            return singleJSXElement !== undefined || singleJSXElement != null;
-        });
+
+        if (appState.availableRepositories.length === 0) {
+            return [(<i>There are no Repositories yet.</i>)]
+        }
+ 
+        let filteredRepositories = appState.availableRepositories?.filter(r => r.name.toLowerCase().includes(repositoryFilterText.toLowerCase()))
+
+        if (filteredRepositories.length === 0) {
+            return [(<i>Please consider using a different filter condition. There are no results.</i>)]
+        }
+
+        return filteredRepositories.map((repository: RepositoryHeaderModel, i) => {
+            return (
+                <ListGroup.Item key={i} action active={repository === selectedRepo} onClick={() => setSelectedRepo(repository)}>{repository.rid === appState.repository?.rid && <i className="bi bi-dot"></i>} {repository.name}</ListGroup.Item>
+            );
+        })
     }
     let repositories = getCurrentRepositoryExpression();
-
 
     return (
         <Container className="main d-flex pt-4 justify-content-center">
@@ -85,11 +98,7 @@ export const Repositories: React.FC = () => {
                         </Row>
 
                         <ListGroup className="my-4" style={{ maxHeight: 'calc(100vh - 250px)' }}>
-                            {appState.availableRepositories.length === 0 ?
-                                <p>There are no Repositories yet.</p> :
-                                repositories.length === 0 ?
-                                    <p>Please consider using a different filter condition. There are no results.</p> :
-                                    repositories}
+                            {repositories}
                         </ListGroup>
                     </Col>
                     <Col className="d-flex flex-column justify-content-between">
@@ -100,8 +109,26 @@ export const Repositories: React.FC = () => {
                             <ButtonGroup className="me-2 mb-2 w-100" vertical><DeleteRepoModal repo={selectedRepo} /></ButtonGroup>
                         </ButtonGroup>
                         <ButtonGroup vertical className="w-100">
-                            <ButtonGroup className="me-2 mb-2 w-100" vertical><Button variant="primary" type="submit" disabled={selectedRepo == null} onClick={chooseRepo}>Select</Button></ButtonGroup>
-                            <ButtonGroup className="me-2 mb-2 w-100" vertical><Button onClick={refresh}><i className="bi bi-arrow-clockwise"></i></Button></ButtonGroup>
+                            <ButtonGroup className="me-2 mb-2 w-100" vertical>
+                                <LoadingButton
+                                    loading={choosing}
+                                    variant="primary"
+                                    type="submit"
+                                    disabled={selectedRepo === null || selectedRepo.rid === appState.repository?.rid}
+                                    onClick={chooseRepo}>Select</LoadingButton>
+                            </ButtonGroup>
+                            <ButtonGroup className="me-2 mb-2 w-100" vertical>
+                                <Button onClick={refresh}>
+                                    {loading ?
+                                        <Spinner
+                                            as="span"
+                                            animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                        /> : <i className="bi bi-arrow-clockwise" />}
+                                </Button>
+                            </ButtonGroup>
                         </ButtonGroup>
                     </Col>
                 </Row>
