57e629e5fec6404e8e802375eddb4c3a7f705d5f
MatthiasPreuner <matthias.preuner@gmail.com>
2022-06-13 11:24:18 +0200

    Network Error considered

--- src/components/common/ErrorResponseToast.tsx
+++ src/components/common/ErrorResponseToast.tsx
@@ -7,13 +7,18 @@ export interface ErrorResponseToastProps {
 }
 
 export const ErrorResponseToast: React.FC<ErrorResponseToastProps> = (props) => {
-
+    console.log(props.error)
     if (props.error === null || props.error === undefined) {
         return (<></>)
+    } else if (props.error.response === undefined) {
+        // usually Network Error
+        return (<Toast bg='warning' className="w-100">
+            <Toast.Body >Network Error</Toast.Body>
+        </Toast>)
     } else {
         return (
             <Toast bg='warning' className="w-100">
-            {/*     <Toast.Header closeButton={false}><b>{props.error.response.status}</b>&nbsp;{props.error.response.data.message}</Toast.Header> */}
+                {/*     <Toast.Header closeButton={false}><b>{props.error.response.status}</b>&nbsp;{props.error.response.data.message}</Toast.Header> */}
                 <Toast.Body >{props.error.response.data._embedded.errors[0].message}</Toast.Body>
             </Toast>
         )
