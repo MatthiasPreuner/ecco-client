2e635833a74d6bbbe8d72b3726491ea67ea47d45
MatthiasPreuner <matthias.preuner@gmail.com>
2022-06-14 14:29:47 +0200

    ErrorToast improved

--- src/components/common/ErrorResponseToast.tsx
+++ src/components/common/ErrorResponseToast.tsx
@@ -7,23 +7,26 @@ export interface ErrorResponseToastProps {
 }
 
 export const ErrorResponseToast: React.FC<ErrorResponseToastProps> = (props) => {
-    if (props.error === null || props.error === undefined) {
+    if (!props.error) {
         return (<></>)
     } else if (props.error.response === undefined) {
         // usually Network Error
-        return (<Toast bg='warning' className="w-100">
-            <Toast.Body >Network Error</Toast.Body>
-        </Toast>)
-    } else {
         return (
             <Toast bg='warning' className="w-100">
-                {/*     <Toast.Header closeButton={false}><b>{props.error.response.status}</b>&nbsp;{props.error.response.data.message}</Toast.Header> */}
+                <Toast.Body >Network Error</Toast.Body>
+            </Toast>
+        )
+    } else if (props.error.response.data._embedded && props.error.response.data._embedded.errors) { // specified by API
+        return (
+            <Toast bg='warning' className="w-100">
                 <Toast.Body >{props.error.response.data._embedded.errors[0].message}</Toast.Body>
             </Toast>
         )
+    } else { // not specified by API
+        return (
+            <Toast bg='warning' className="w-100">
+                <Toast.Body ><b>{props.error.response.status}</b>&nbsp;{props.error.response.statusText}</Toast.Body>
+            </Toast>
+        )
     }
-
-
-
-
 }
