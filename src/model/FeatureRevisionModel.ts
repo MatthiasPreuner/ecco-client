dfda95153537629feb98c41c1bb7e1ea3187ee5a
Matthias Preuner <matthias.preuner@gmail.com>
2022-07-04 13:09:12 +0200

    code cleanup

--- src/model/FeatureRevisionModel.ts
+++ src/model/FeatureRevisionModel.ts
@@ -1,7 +1,7 @@
 export interface FeatureRevisionModel {
     id: string,
     description: string,
-    featureRevisionString: string, // TODO remove ??
+    featureRevisionString: string,
     featureName: string,
     featureRevisionIndex: string
 }
